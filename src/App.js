531b40e517c1ce5c073c93efeb7ef89fec0a7321
Dan Reeves <hey@danreev.es>
2017-08-20 14:50:49 +0100

    autosizing virtualized table, recursively fetch anime

--- src/App.js
+++ src/App.js
@@ -1,52 +1,55 @@
 /* global fetch */
-import React, { Component } from 'react'
-import './App.css'
-import { WatchList } from './containers/watchlist'
+import React, { Component } from 'react';
+import './App.css';
+import 'react-virtualized/styles.css';
+import { WatchList } from './containers/watchlist';
 
 class App extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       username: null,
-      userid: null
-    }
-    this.fetchUser = this.fetchUser.bind(this)
+      userid: null,
+    };
+    this.fetchUser = this.fetchUser.bind(this);
   }
 
-  async fetchUser (e) {
-    const name = e.target.value
-    const resp = await fetch(`https://kitsu.io/api/edge/users?filter[name]=${name}`)
-    const data = await resp.json()
+  async fetchUser(e) {
+    const name = e.target.value;
+    const resp = await fetch(
+      `https://kitsu.io/api/edge/users?filter[name]=${name}`
+    );
+    const data = await resp.json();
     if (data.data.length > 0) {
       this.setState((prev, props) => ({
         ...prev,
         username: name,
-        userid: data.data[0].id
-      }))
+        userid: data.data[0].id,
+      }));
     } else {
       this.setState((prev, props) => ({
         ...prev,
         username: name,
-        userid: null
-      }))
+        userid: null,
+      }));
     }
   }
 
-  componentDidMount () {
-    this.fetchUser({ target: { value: 'danreeves' } })
+  componentDidMount() {
+    this.fetchUser({ target: { value: 'danreeves' } });
   }
 
-  render () {
-    const { username, userid } = this.state
+  render() {
+    const { username, userid } = this.state;
     return (
-      <div className='App'>
-        <input type='text' onChange={this.fetchUser} />
+      <div className="App">
+        <input type="text" onChange={this.fetchUser} />
         {username && userid
           ? <WatchList userid={userid} username={username} />
           : <p>User not found</p>}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/containers/watchlist.js
+++ src/containers/watchlist.js
@@ -1,52 +1,90 @@
 /* global fetch */
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { AutoSizer, Column, Table } from 'react-virtualized';
+import queue from 'queue';
+
+async function fetchPage(url) {
+    const resp = await fetch(url);
+    const json = await resp.json();
+    return json;
+}
 
 export class WatchList extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      anime: []
+    constructor(props) {
+        super(props);
+        this.queue = queue({
+            concurrency: props.concurrency || 2,
+            autostart: true,
+        });
+        this.state = {
+            anime: [],
+        };
+        this.fetchAnime = this.fetchAnime.bind(this);
+        this.getShow = this.getShow.bind(this);
     }
-    this.fetchAnime = this.fetchAnime.bind(this)
-  }
 
-  componentWillMount () {
-    const { userid } = this.props
-    this.fetchAnime(userid)
-  }
-
-  async fetchAnime (userid) {
-    const resp = await fetch(`https://kitsu.io/api/edge/users/${userid}/library-entries?filter[status]=planned`)
-    const json = await resp.json()
-    const shows = await Promise.all(json.data.map(async entry => {
-      const showresp = await fetch(entry.relationships.anime.links.related)
-      const showdata = await showresp.json()
-      return {
-        show: showdata.data ? showdata.data.attributes : null,
-        attrs: entry.attributes
-      }
-    }))
-    if (shows.length) {
-      this.setState(prev => ({
-        anime: shows
-      }))
+    componentWillMount() {
+        const { userid } = this.props;
+        const firstPage = `https://kitsu.io/api/edge/users/${userid}/library-entries?filter[status]=planned`;
+        this.fetchAnime(firstPage);
     }
-  }
 
-  render () {
-    const { username } = this.props
-    const { anime } = this.state
-    return <div>
-      <h2>Watchlist for {username}</h2>
-      <ul>
-        {anime.map(show => {
-          if (show.show) {
-            return <li>{show.show.canonicalTitle}</li>
-          } else {
-            return null
-          }
-        })}
-      </ul>
-    </div>
-  }
+    async fetchAnime(next) {
+        this.queue.push(async () => {
+            const { data: shows, links } = await fetchPage(next);
+            shows.forEach(this.getShow);
+            if (links.next) {
+                await this.fetchAnime(links.next);
+            }
+        });
+    }
+
+    getShow(showmeta) {
+        this.queue.push(async () => {
+            const showinfo = await fetchPage(
+                showmeta.relationships.anime.links.related
+            );
+            const info = showinfo.data ? showinfo.data.attributes : {};
+            const show = {
+                ...showmeta.attributes,
+                ...info,
+            };
+            this.setState(prev => ({
+                anime: [...prev.anime, show],
+            }));
+        });
+    }
+
+    render() {
+        const { username } = this.props;
+        const { anime } = this.state;
+        return (
+            <div className="full">
+                <h2>
+                    Watchlist for {username}
+                </h2>
+                <div className="full">
+                    <AutoSizer>
+                        {({ height, width }) =>
+                            <Table
+                                height={height}
+                                width={width}
+                                headerHeight={20}
+                                rowHeight={30}
+                                rowCount={anime.length}
+                                rowGetter={({ index }) => anime[index]}
+                            >
+                                <Column
+                                    label="Title"
+                                    dataKey="canonicalTitle"
+                                    width={100}
+                                    flexGrow={1}
+                                    disableSort={false}
+                                />
+                            </Table>}
+                    </AutoSizer>
+                </div>
+            </div>
+        );
+    }
 }
